53f9c44701e7e9d3f694f67064a16d9ebf3b5980
qhfan07 <qhfan07@gmail.com>
2024-12-04 17:14:17 -0800

    Modifiy

--- src/CatFacts.jsx
+++ src/CatFacts.jsx
@@ -1,38 +1,46 @@
 import axios from 'axios';
 import CatLogo from './assets/cat.svg'
 import './CatFacts.css';
+import React, { useState, useEffect } from 'react';
 
-// insert your name here
+// Qianhao Fan
 
 function CatFacts() {
+    const [catFact, setCatFact] = useState("Loading...");
+    const [isLoading, setIsLoading] = useState(false);
 
-    // you may need to add other code elsewhere!
-
-    function generateCatFact() {
-        axios.get('https://catfact.ninja/fact')
-            .then(response => {
-                // insert code here
-            })
+    async function generateCatFact() {
+        setIsLoading(true);
+        try {
+            const response = await axios.get('https://catfact.ninja/fact');
+            setCatFact(response.data.fact);
+        } catch (error) {
+            setCatFact("Failed to fetch a cat fact. Please try again!");
+        } finally {
+            setIsLoading(false);
+        }
     }
 
+    useEffect(() => {
+        generateCatFact();
+    }, []);
 
-
-  return (
-    <div className="App">
-        <div className='catFactsText'>
-            {"Loading..." }
-            {/* The cat fact should be displayed here*/} 
+    return (
+        <div className="App">
+            <div className='catFactsText'>
+                {isLoading ? "Loading..." : catFact}
+                {/* The cat fact should be displayed here*/} 
+            </div>
+            <div>
+                <button onClick={generateCatFact} className="catFactBtn">
+                    Click me for a cat fact!
+                </button>
+            </div>
+            <div>
+                <img src={CatLogo} className="catFactImg" />
+            </div>
         </div>
-        <div>
-            <button onClick={generateCatFact} className="catFactBtn">
-                Click me for a cat fact!
-            </button>
-        </div>
-        <div>
-            <img src={CatLogo} className="catFactImg" />
-        </div>
-    </div>
-  )
-}
+    )
+    }
 
 export default CatFacts
